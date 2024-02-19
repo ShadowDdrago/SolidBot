6992e4c4416c2e86cc08e3beae9a130c74c2cbc6
ShadowDdrago <123237132+ShadowDdrago@users.noreply.github.com>
2024-02-19 06:06:53 +0300

    Create confi.py

---
+++ confi.py
@@ -0,0 +1,9 @@
+TOKEN = "MTA3NzA4NzEzMTUzMDQ0ODg5Ng.Geq9ce.sKyIu7JRRcW7XfL7DI51P-2gS1160UooAAhSv0"
+config = {'user': 'u168073_c0nswljPxY', 
+          'password': '.pa+mxTzsQ+Mrp^Ec3MUXGb1',
+          'host': 'mysql4.joinserver.xyz',
+          'database':'s168073_kjabdgkjabkgb'
+          }
+host = '95.216.48.139' 
+port = 25655
+passwd="zHuXcRBjSe"
