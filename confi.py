14a339b7aa01a039be7c25e0b1aa290e59070aa0
ShadowDdrago <123237132+ShadowDdrago@users.noreply.github.com>
2024-06-23 14:10:41 +0300

    Update confi.py

--- confi.py
+++ confi.py
@@ -1,4 +1,4 @@
-TOKEN = ""
+TOKEN = "MTA3NzA4NzEzMTUzMDQ0ODg5Ng.GDZM-B.H2Bw-AiB7Ar9Vr6KQA4JLrycOW-4G3jzl2bRnM"
 config = {'user': 'u168073_rKIwWJaoaO', 
           'password': 'WmY=ZYWBm9Sxoqj.6M3s3.j1',
           'host': 'mysql.joinserver.xyz',
