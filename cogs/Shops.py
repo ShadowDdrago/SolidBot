f006cd1f59966319dba99b877dbef66dbcfa44ea
ShadowDdrago <123237132+ShadowDdrago@users.noreply.github.com>
2024-06-25 13:57:20 +0300

    Update Shops.py

--- cogs/Shops.py
+++ cogs/Shops.py
@@ -12,26 +12,37 @@ from confi import config
 class Shops(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:      
         self.bot = bot  
+#СОЗДАНИЕ ВСЕХ КНОПОК
+    # ОСНОВНАЯ КНОПКА
     global PersistentView
     class PersistentView(disnake.ui.View):
         def __init__(self):
             super().__init__(timeout=None)
-
-        @disnake.ui.button(
-            label="Green", style=disnake.ButtonStyle.green, custom_id="persistent_example:green"
-        )
-        async def green(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
-            Wardrob = ['<:greenpoint:1255098975208607805> Туманность \n \
+            self.wardrob =  ['<:greenpoint:1255098975208607805> Туманность \n \
                         <:yellowpoint:1255098956321521807> Неко тян\n\
                         <:yellowpoint:1255098956321521807> Dota 2 \n\
                         <:greenpoint:1255098975208607805> Valorant \n\
                         <:greenpoint:1255098975208607805> Закат \n\
-                        ']
-            wardrobe_embed = disnake.Embed()
-            wardrobe_embed.add_field(name = "**Фоны профыиля**" ,
-                                     value=f"{Wardrob[0]}" )
+                        ','АБОБАББАА 1', 'АБОБАААА 2']
+            self.count = 0
+            self.wardrobe_embed = disnake.Embed()
+
+        @disnake.ui.button(
+            label="Гардироб", style=disnake.ButtonStyle.green
+        )
+        async def green(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
+            self.wardrobe_embed.add_field(name = "**Фоны профыиля**" ,
+                                     value=f"{self.wardrob[0]}" )
             emojis = inter.message.guild
-            await inter.send(embed = wardrobe_embed, ephemeral=True)
+            await inter.send(embed = self.wardrobe_embed, ephemeral=True)
+        @disnake.ui.button(
+            label="Next", style=disnake.ButtonStyle.green
+        )
+        async def next(self, button: disnake.ui.Button, inter: disnake.MessageInteraction):
+            self.count += 1
+            self.wardrobe_embed.remove_field(0)
+            self.wardrobe_embed.add_field(name = "**Фоны профыиля**" ,
+                                     value=f"{self.wardrob[self.count]}")
     @commands.Cog.listener()
     async def on_ready(self):
         if not self.bot.persistent_views:
