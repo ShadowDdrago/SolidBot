afc725169584a85daef2324e83abe5539e6492c2
ShadowDdrago <123237132+ShadowDdrago@users.noreply.github.com>
2024-06-25 13:39:31 +0300

    Update Shops.py

--- cogs/Shops.py
+++ cogs/Shops.py
@@ -48,31 +48,6 @@ class Shops(commands.Cog):
         shop=File(fp=fp , filename="Solid.png")
         
         await inter.send(file=shop, view = PersistentView())
-    @commands.Cog.listener()
-    async def on_button_click(self, inter: disnake.MessageInteraction):
-        
-        member = inter.author
-        custom = inter.component.custom_id
-        if custom == "wardrobe":
-            Wardrob = ['<:greenpoint:1255098975208607805> Туманность \n \
-                        <:yellowpoint:1255098956321521807> Неко тян\n\
-                        <:yellowpoint:1255098956321521807> Dota 2 \n\
-                        <:greenpoint:1255098975208607805> Valorant \n\
-                        <:greenpoint:1255098975208607805> Закат \n\
-                        ', 
-                        '']
-            wardrobe_embed = disnake.Embed()
-            wardrobe_embed.add_field(name = "**Фоны профыиля**" ,
-                                     value=f"\
-                                     <:greenpoint:1255098975208607805> Туманность \n 
-                                     <:yellowpoint:1255098956321521807> Неко тян\n\
-                                     <:yellowpoint:1255098956321521807> Dota 2 \n\
-                                     <:greenpoint:1255098975208607805> Valorant \n\
-                                     <:greenpoint:1255098975208607805> Закат \n\
-                                     " )
-            emojis = inter.message.guild
-            await inter.send(embed = wardrobe_embed, ephemeral=True)
-            next_point = Button(style  = ButtonStyle.blurple,  label = "next",custom_id = "wardrobe")
             
             
             
